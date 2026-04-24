c67b20b4eb81aad752a98d9152b3132cdff3a928
misba <misbartszek@gmail.com>
2026-04-24 12:35:23 +0200

    docker-compose improvment

--- src/main/java/org/example/banksimulation/controllers/ChaosController.java
+++ src/main/java/org/example/banksimulation/controllers/ChaosController.java
@@ -1,13 +1,21 @@
 package org.example.banksimulation.controllers;
 
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChaosController {
 
+    private final ConfigurableApplicationContext context;
+
+    public ChaosController(ConfigurableApplicationContext context) {
+        this.context = context;
+    }
+
     @PostMapping("/chaos")
-    public void chaos() {
-        System.exit(0);
+    public void  chaos() {
+        context.close();
+        
     }
 }
